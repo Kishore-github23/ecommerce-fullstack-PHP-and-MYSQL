1880554046a8e19b97d7510f04927f61b43c9666
kishorekumarjothikumar@gmail.com <kishorekumarjothikumar@gmail.com>
2024-09-15 17:01:25 +0530

    Initial commit

---
+++ xampp/htdocs/TRONICS ZONE/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CRoboto" rel="stylesheet">
+  <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="shortcut icon" href="https://as2.ftcdn.net/v2/jpg/03/98/50/73/1000_F_398507328_uTotT06EubtdODpxSaHL1M1hdB3paXkL.jpg" type="image/png">
+  <title>TRONICS ZONE</title>
+  <link href="styles/bootstrap.min.css" rel="stylesheet">
+  <link href="styles/backend.css" rel="stylesheet">
+  <link href="styles/style.css" rel="stylesheet">
+
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
